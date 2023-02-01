cdb4d86a836dd993cf41110f74016a33662cf665
Your Name <you@example.com>
2023-02-01 12:54:35 +0100

    Email checking

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,8 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Get, Post, Render } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Post, Render } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { AppService } from './app.service';
+import RegisterDto from './register.dto';
 
 @Controller()
 export class AppController {
@@ -14,5 +15,12 @@ export class AppController {
   @Render('index')
   index() {
     return { message: 'Welcome to the homepage' };
+  }
+
+  @Post('/register')
+  register(@Body() registerDto: RegisterDto) {
+    if (!registerDto.email.includes('@')) {
+      throw new BadRequestException('Email must contain a @ character');
+    }
   }
 }
