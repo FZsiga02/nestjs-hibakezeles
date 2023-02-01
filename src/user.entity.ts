08ef138f4792567072d193085445f75a2737800a
Your Name <you@example.com>
2023-02-01 12:23:22 +0100

    User entity

---
+++ src/user.entity.ts
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Url } from "url";
+
+@Entity()
+export default class User{
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  profilePictureUrl: Url;
+
+  @Column()
+  registrationDate: Date;
+}
+
